e99be05477d27955a9ccd366b84977b1e3e1e89a
danielepucci03 <danielepucci03@gmail.com>
2026-03-25 18:25:45 +0100

    add function equal number

--- main 2/main.js
+++ main 2/main.js
@@ -4,4 +4,7 @@ function generaNumeroRandom() {
   return Math.floor(Math.random() * 5) + 1;
 }
 
-
+// Funzione per stabilire se un numero è pari
+function isPari(numero) {
+  return numero % 2 === 0;
+}
