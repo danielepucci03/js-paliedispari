ae7e1f5735a6c7406fe43b6d32b3fbf8d12df445
danielepucci03 <danielepucci03@gmail.com>
2026-03-25 18:27:34 +0100

    add computer number

--- main 2/main.js
+++ main 2/main.js
@@ -12,3 +12,6 @@ function isPari(numero) {
 // INPUT UTENTE
 const scelta = prompt("Scegli: pari o dispari").toLowerCase();
 const numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
+
+// Numero computer
+let numeroComputer = generaNumeroRandom();
