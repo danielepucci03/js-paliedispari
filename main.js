6fc64dd2e2bd584317fa185a6c095386010d9ca0
danielepucci03 <danielepucci03@gmail.com>
2026-03-25 18:02:27 +0100

    add confront

--- main.js
+++ main.js
@@ -3,11 +3,19 @@ console.log('PARI E DISPARI');
 // Creo la funzione e metto tutto in minuscolo per non avere problemi
 function palindroma (parola) {
     parola = parola.toLowerCase ()
+
+
+
+    // Inverto la parola
+    const parolaInvertita = parola.reverse('')
+
+
+    // Confronto
+    if (parola === parolaInvertita) {
+        return true
+    } else {
+        return false
+        }
+
 }
-
-
-//Inverto la parola
-const parolaInvertita = parola.reverse('')
-
-
 
